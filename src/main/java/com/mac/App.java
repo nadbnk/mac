84aca4f6a2b54e4543235ec85cea62a86794fe7f
root <root@ip-172-31-18-36.us-east-2.compute.internal>
2021-12-09 04:24:17 +0000

    mac

---
+++ src/main/java/com/mac/App.java
@@ -0,0 +1,13 @@
+package com.mac;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/mac/AppTest.java
@@ -0,0 +1,20 @@
+package com.mac;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
